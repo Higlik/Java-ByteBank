3285e52a187f3b68588942b77185e74832e28a7b
Higlik <victor.silva.franca18@gmail.com>
2023-01-17 11:04:42 -0300

    initial commit

---
+++ ByteBank/src/model/Account.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Account {
+	
+	private int agency;
+	private int number;
+	private double balance;
+	private String holder;
+
+}
---
+++ ByteBank/src/view/main.java
@@ -0,0 +1,17 @@
+package view;
+
+import model.Account;
+
+public class main {
+	
+	public main() {
+		// TODO Auto-generated constructor stub
+		
+		Account ac = new Account();
+		
+		ac.balance = 200;
+		
+		
+	}
+
+}
